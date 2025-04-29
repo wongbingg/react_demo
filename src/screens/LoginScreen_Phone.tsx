32b9166e3adb6e0ba48ed3e92790e4226ad22ad0
WonbeenLee <wblee@autocrypt.io>
2025-04-29 16:31:45 +0900

    feat : 새로운 로그인 화면 추가

---
+++ src/screens/LoginScreen_Phone.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import CustomNavigationBar from './components/CustomNavigationBar'
+import { AuthRepository } from '../network/authRepository'
+import { MyAsyncStorage } from '../persistance/asyncStorage'
+
+type LoginScreen_PhoneProp = NativeStackScreenProps<RootStackParamList, 'Login_Phone'>
+
+export default function LoginScreen_Phone({ navigation }: LoginScreen_PhoneProp) {
+
+    const [inputNumber, setInputNumber] = useState('')
+    const [inputPassword, setInputPassword] = useState('')
+
+    const handleNextButton = async () => {
+        try {
+            const res = await AuthRepository.login({phoneNo: inputNumber, password: inputPassword, token: 'empty'})
+            if (res.code === 0) {
+                console.log('로그인 성공')
+                MyAsyncStorage.save('IS_LOGIN', 'Y')
+                navigation.goBack();
+            } else {
+                console.error('로그인 실패', res.msg)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const handleSendButton = () => {
+        AuthRepository.sendCode({ phoneNo: inputNumber })
+    }
+
+    return (
+        <View style={styles.container}>
+            {/* 네비게이션바 */}
+            <CustomNavigationBar
+                title={'로그인'}
+                isBack={true}
+            />
+
+            <Text
+                style={{
+                    padding: 20,
+                    marginTop: 40,
+                }}
+            >
+                전화번호를 입력해주세요.
+            </Text>
+            <View
+                style={{ flexDirection: 'row', gap: 20, alignItems: 'center' }}
+            >
+                <TextInput
+                    placeholder='전화번호 입력'
+                    style={styles.textInput}
+                    keyboardType='numeric'
+                    onChangeText={setInputNumber}
+                />
+                <Button
+                    title="보내기"
+                    onPress={handleSendButton}
+                />
+            </View>
+            <TextInput
+                placeholder='비밀번호 입력'
+                style={styles.textInput}
+                onChangeText={setInputPassword}
+            />
+            <View style={{ flex: 1 }} />
+
+            <Button
+                title="다음"
+                disabled={false}
+                onPress={handleNextButton}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start'
+    },
+    textInput: {
+        fontSize: 20,
+        marginLeft: 20,
+        width: '70%',
+        borderBottomWidth: 1, // Underline 두께
+        borderBottomColor: '#000', // Underline 색상
+        marginVertical: 10, // 간격 추가
+        padding: 5, // 텍스트 입력 패딩
+    },
+})
