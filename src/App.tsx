0c9ee03baf96879b6ae1d81b7af426f5aee0b4a8
WonbeenLee <wblee@autocrypt.io>
2025-04-29 16:32:52 +0900

    feat : 앱 시작시, 로그인 유효성 검사 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@
 //  * @format
 //  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { NavigationContainer } from '@react-navigation/native'
@@ -15,19 +15,46 @@ import HomeScreen from './screens/HomeScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import WriteScreen from './screens/WriteScreen';
 import MemoScreen from './screens/MemoScreen';
+import LoginScreen_Phone from './screens/LoginScreen_Phone';
+import { AuthRepository } from './network/authRepository';
+import SignUpScreen from './screens/SignUpScreen';
+import { MyAsyncStorage } from './persistance/asyncStorage';
 
 export type RootStackParamList = {
   Login: undefined;
+  Login_Phone: undefined;
+  SignUp: undefined;
   Home: undefined;
   Profile: undefined;
   Write: undefined;
   Memo: { id: string };
-  Example: undefined;
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
+
+  // mark; 앱 런칭 타이밍
+  useEffect(() => {
+    const checkAccessToken = async () => {
+      try {
+        const accessToken = await MyAsyncStorage.load('ACCESS_TOKEN')
+        const res = await AuthRepository.loginValidate({ accessToken: accessToken, token: 'empty' })
+
+        if (res.code === 0) {
+          // todo; 홈스크린으로 이동 
+          MyAsyncStorage.save('IS_LOGIN', 'Y')
+        } else {
+          // todo; 로그인화면 유지
+          MyAsyncStorage.save('IS_LOGIN', 'N')
+        }
+      } catch (err) {
+        MyAsyncStorage.save('IS_LOGIN', 'N')
+        console.error(err);
+      }
+    }
+    checkAccessToken()
+  }, [])
 
   return (
     <Provider store={store}>
@@ -65,6 +92,14 @@ export default function App() {
           <Stack.Screen
             name="Memo"
             component={MemoScreen}
+          />
+          <Stack.Screen
+            name="Login_Phone"
+            component={LoginScreen_Phone}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
           />
         </Stack.Navigator>
       </NavigationContainer>
